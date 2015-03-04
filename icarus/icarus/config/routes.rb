b9e1c5c24cb8662a790f0b2b3af86226b9e8a2b5
Rick Gibson <ricklesgibson@gmail.com>
2015-03-03 16:47:58 -0800

    Built out controllers and basic NEW views for models

--- icarus/icarus/config/routes.rb
+++ icarus/icarus/config/routes.rb
@@ -1,31 +1,6 @@
 Rails.application.routes.draw do
-  get 'owners/new'
-
-  get 'owners/index'
-
-  get 'owners/edit'
-
-  get 'owners/show'
-
-  get 'owners/create'
-
-  get 'owners/update'
-
-  get 'owners/destroy'
-
-  get 'pilots/index'
-
-  get 'pilots/new'
-
-  get 'pilots/show'
-
-  get 'pilots/create'
-
-  get 'pilots/update'
-
-  get 'pilots/edit'
-
-  get 'pilots/destroy'
+  resources :owners
+  resources :pilots
 
   resource :sessions, only: [:new, :create, :destroy] #this is a singular resource, not resources.
   resources :users do
