c553470180a6bf91cbf9a25d06f319cce474b5dc
Rick Gibson <ricklesgibson@gmail.com>
2015-03-05 15:53:41 -0800

    Add fields to Events

--- icarus/icarus/db/schema.rb
+++ icarus/icarus/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304230257) do
+ActiveRecord::Schema.define(version: 20150305234759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20150304230257) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
