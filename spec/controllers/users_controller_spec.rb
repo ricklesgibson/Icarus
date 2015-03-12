2bf513ba29a4b61b0d12b6d93a250d3dbc92dd20
Adam McLean <adam.mclean1@gmail.com>
2015-03-12 16:54:01 -0700

    finish controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -40,10 +40,12 @@ end
 
 
   describe "POST #create" do
-    it "posts to the database and is going to have a good time" do
-      expect{User.create}.to change(User, :count).by(1)
+    it "UNLIKE 'new,' we POST the user info to the DATABASE" do
+      expect{post :create, user: valid_attributes}.to change(User, :count).by(1)
     end
+
   end
+
 
   describe "GET #destroy" do
     before do
@@ -79,22 +81,43 @@ end
 
   describe "GET #edit" do
     before :each do
-      @user=User.create valid_attributes
-      get :edit, id: @user.id
+      @test_user=User.create valid_attributes
+      get :edit, id: @test_user.id
     end
 
-    it "assigns user to @user and returns http success" do
+    it "renders edit template and returns http success" do
       get :edit
       expect(response).to render_template :edit
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
+  describe "PUT/PATCH #update" do
+    before do
+      @test_user = User.create valid_attributes
+      get :edit, id: @test_user.id
   end
 
-end
+  describe "with successful update" do
+    let :new_attributes do
+      {
+          :email => ""
+          :first_name => ""
+          :last_name => ""
+          :street_address => ""
+          :city => ""
+          :state => ""
+          :zip => ""
+      }
+    end
+
+    before do
+      patch :update, id: @test_user.id
+    end
+
+    it "should redirect to the show path for this user" do
+      expect(response).to render_template :show
+      expect(response).to redirect_to item_path
+    end
+
+  end
